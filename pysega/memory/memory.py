b0a39eae27158e6ef6a8e55811dc17babb75f57a
Andrew <andrew@localhost>
2018-01-14 13:37:08 +1100

    Updated memory read/write calls. Page switching copies to an array during register changes.  Slows writes, but speeds up reads.

--- pysega/memory/memory.py
+++ pysega/memory/memory.py
@@ -28,6 +28,10 @@ class Memory(object):
 
         self._ram     = [0] * self.RAM_SIZE
 
+        # 'cache' of memory.  Copy of memory to ready from, updated when paging changes.
+        self._cached_read = [0] * self.MEMMAPSIZE
+        self._pages = [None] * 4
+
         # Complete memory map
         self._memory_map     = None
 
@@ -37,7 +41,7 @@ class Memory(object):
         self._memory_map     = [0] * self.MEMMAPSIZE
 
     def read(self, address):
-        return self._translate_read(address)
+        return self._cached_read[address & 0xFFFF]
 
     def readArray(self, address, length):
 
@@ -63,15 +67,123 @@ class Memory(object):
             self.write(dest+i, self.read(src+i))
 
     def write(self, address, data):
-        self._translate_write(address, int(data))
+        # self._translate_write(address, int(data))
+        self._cache_write(address, int(data))
 
     def set_cartridge(self, cartridge):
         self.cartridge = cartridge
 
         self._initialise_memory()
+        self._update_fixed_read()
 
     def set_z80memory(self, z80memory):
         self.z80memory = z80memory
+
+    def _update_fixed_read(self):
+        """ Un-optimised address translation, uses paging registers. 
+        """
+
+        self._update_fixed_read_page0()
+        self._update_fixed_read_page1()
+        self._update_fixed_read_page2()
+        self._update_fixed_read_ram()
+
+    def _update_fixed_read_page0(self):
+        bank_array = self.cartridge.cartridge_banks[0]
+        for address in range(self.PAGE0):
+            bank_address = address & 0x3FFF # BANK_MASK
+            self._cached_read[address] = bank_array[bank_address]
+
+    def _update_fixed_read_page1(self):
+        page0_bank = self._memory_map[0xFFFD]
+        bank_array = self.cartridge.cartridge_banks[page0_bank]
+        for address in range(self.PAGE0, self.PAGE1):
+            bank_address = address & 0x3FFF # BANK_MASK
+            self._cached_read[address] = bank_array[bank_address]
+
+    def _update_fixed_read_page2(self):
+        page1_bank = self._memory_map[0xFFFE]
+        bank_array = self.cartridge.cartridge_banks[page1_bank]
+        for address in range(self.PAGE1, self.PAGE2):
+            bank_address = address & 0x3FFF # BANK_MASK
+            self._cached_read[address] = bank_array[bank_address]
+
+    def _update_fixed_read_ram(self):
+        ram_select = self._memory_map[0xFFFC]
+
+
+        if (ram_select & 0x8): # page2_is_cartridge_ram
+          if (ram_select & 0x4):
+            cartridge_ram_page = 1
+          else:
+            cartridge_ram_page = 0
+      
+          bank_array = self.cartridge.ram[cartridge_ram_page]
+          for address in range(self.PAGE2, self.RAM_OFFSET):
+              bank_address = address & 0x3FFF # BANK_MASK
+              self._cached_read[address] = bank_array[bank_address]
+
+        else:
+          page2_bank = self._memory_map[0xFFFF]
+          bank_array = self.cartridge.ram[page2_bank]
+          for address in range(self.PAGE2, self.RAM_OFFSET):
+              bank_address = address & 0x3FFF # BANK_MASK
+              self._cached_read[address] = bank_array[bank_address]
+
+        for address in range(self.RAM_OFFSET, self.PAGING_REGISTERS):
+            # This covers RAM & Mirrored RAM
+            self._cached_read[address] = self._ram[address & (self.RAM_SIZE - 1)]
+
+    def _cache_write(self, address, data):
+        """ Perform write.
+            Copy write to the 'cached' memory array.
+            If a paging selection register is written, refresh the 'cached' memory array.
+        """
+
+        address = address & 0xFFFF # ADDRESS_MASK
+        bank_address = address & 0x3FFF # BANK_MASK
+
+        self._cached_read[address] = data
+
+        if(address >= self.PAGING_REGISTERS):
+            self._memory_map[address] = data
+
+            # self._update_fixed_read_page0()
+            if (address == 0xFFFD):
+                if (data != self._pages[1]):
+                    self._update_fixed_read_page1()
+                    self._pages[1] = data
+            elif (address == 0xFFFE):
+                if (data != self._pages[2]):
+                    self._update_fixed_read_page2()
+                    self._pages[2] = data
+            elif (address == 0xFFFC):
+                if (data != self._pages[3]):
+                    self._update_fixed_read_ram()
+                    self._pages[3] = data
+        elif(address < self.PAGE0):
+            self.cartridge.cartridge_banks[0][bank_address] = data
+        elif(address < self.PAGE1):
+            page0_bank = self._memory_map[0xFFFD]
+            self.cartridge.cartridge_banks[page0_bank][bank_address] = data
+        elif(address < self.PAGE2):
+            page1_bank = self._memory_map[0xFFFE]
+            self.cartridge.cartridge_banks[page1_bank][bank_address] = data
+        elif(address < self.RAM_OFFSET):
+            ram_select = self._memory_map[0xFFFC]
+            if (ram_select & 0x8): # page2_is_cartridge_ram
+              if (ram_select & 0x4):
+                cartridge_ram_page = 1
+              else:
+                cartridge_ram_page = 0
+      
+              self.cartridge.ram[cartridge_ram_page][bank_address] = data
+            else:
+              page2_bank = self._memory_map[0xFFFF]
+              self.cartridge.cartridge_banks[page2_bank][bank_address] = data
+        elif(address < self.PAGING_REGISTERS):
+            # This covers RAM & Mirrored RAM
+            self._ram[address & (self.RAM_SIZE - 1)] = data
 
     def _translate_read(self, address):
         """ Un-optimised address translation, uses paging registers. 
@@ -108,6 +220,7 @@ class Memory(object):
             result = self._memory_map[address]
 
         return result
+
 
     def _translate_write(self, address, data):
         """ Un-optimised address translation, uses paging registers. 
